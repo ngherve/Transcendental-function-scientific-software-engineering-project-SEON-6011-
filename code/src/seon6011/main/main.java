e97c4c53752ddfd7355b8c11847d5d06fdfad23f
ngherve <ngomseuherve@gmail.com>
2022-07-16 14:34:55 -0400

    Adding the context model and requirements extractions

--- code/src/seon6011/main/main.java
+++ code/src/seon6011/main/main.java
@@ -5,32 +5,39 @@ package seon6011.main;
  *
  */
 public class main {
+	
+	
+	/**
+	 * @param args Commnd line arguments
+	 * @throws Exception arithmetic exception erro
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println(calculate(2, E, EXPANSION));
+	}
 		
 	/**
-	 * Calculate absolute value of the input number.
+	 * Calculate absolute value of an integer.
 		///FOR TESTING
-	 * @param x double type
-	 * @return absolute value of x, double type
+	 * @param value double value
+	 * @return absolute value of value, double type
 	 */
-	public static double abs(double x) {
-		if (x < 0) {
-			x = -x;
-	    }
-	    return x;
+	public static double abs(double value) {
+		if (value < 0) value = -value;
+		return value;
 	}
 	/*
-	 * Calculate the value of e^x.
+	 * Calculate the value of e^value (Exponential function) using Taylor series expansion.
 		///FOR TESTING
-	 * @param x exponent number,double type
-	 * @param n the number of Taylor expansions,integer type
-	 * @return e^x double type
+	 * @param value exponent number,double type
+	 * @param nterms the number of Taylor expansions,integer type
+	 * @return e^value double type
 	 */
-	 public static double ex(double x, int n) {
+	 public static double ex(double value, int nterms) {
 		 double result = 1.0;
 		 double tmp = 1;
-		 for (int i = 1; i <= n; i++) {
+		 for (int i = 1; i <= nterms; i++) {
 			 tmp *= i;
-			 result += power(x, i) / tmp;
+			 result += power(value, i) / tmp;
 		 }
 		 return result;
 	 }
@@ -47,32 +54,32 @@ public class main {
 	 public static final double LN2 = 0.6931471805599453; // value of ln(2)
 	 
 	 /*
-	  * Calculate the value of a*(b^x).
+	  * Calculate the value of a*(b^value).
 	  *
 	  * @param a double type
 	  * @param b base number,double type
-	  * @param x exponent number,double type
-	  * @return a*(b^x) double type
+	  * @param value exponent number,double type
+	  * @return a*(b^value) double type
 	  * @throws Exception the exception
 	  */
-	 public static double calculate(double a, double b, double x) throws Exception {
+	 public static double calculate(double a, double b, double value) throws Exception {
 		 if (a == 0) {
 			 return 0;
 		 }
-		 return a * power(b, x);
+		 return a * power(b, value);
 	 }
 	 
 	
 	 /**
 	  * 
-	  * calculating the power of a number a^x
+	  * calculating the power of a number a^value
 	  * @param value the given base number, a
-	  * @param power the exponent, x base of the base
+	  * @param power the exponent, value base of the base
 	  */
-	 public static double power(double a, int x) {
-		 if (x == 0) return 1.0;
+	 public static double power(double a, int value) {
+		 if (value == 0) return 1.0;
 		 if (a == 0) return 0.0;
-		 int b = x;
+		 int b = value;
 		 if (b < 0) b = -b;
 
 		 double r = a;
@@ -87,7 +94,7 @@ public class main {
 				 y = y * r;
 		     }
 		 }
-		 if (x < 0) {
+		 if (value < 0) {
 			 return 1 / y;
 		 } else {
 			 return y;
@@ -95,17 +102,17 @@ public class main {
 	 }
 
 	 /**
-	  * Calculate the value of a^x.According to a^x=e^(x*ln(a))
+	  * Calculate the value of a^value.According to a^value=e^(value*ln(a))
 	  *
 	  * @param a base number,double type
-	  * @param x exponent number,double type
-	  * @return a^x double type
+	  * @param value exponent number,double type
+	  * @return a^value double type
 	  * @throws Exception the exception
 	  */
-	 public static double power(double a, double x) throws Exception {
-		 if (x == 0) return 1.0;
+	 public static double power(double a, double value) throws Exception {
+		 if (value == 0) return 1.0;
 		 if (a == 0) return 0.0;
-		 double exp = x * ln(a); // If the value of exp is large, substituting exp into the function
+		 double exp = value * ln(a); // If the value of exp is large, substituting exp into the function
 		 // ex may cause the calculation to overflow.
 		 int integer = (int) exp;
 		 double decimal = exp - integer;
@@ -115,38 +122,38 @@ public class main {
 	
 	 
 	 /**
-	  * Calculate the value of ln(x).
+	  * Calculate the value of ln(value).
 	  *
-	  * @param x double type
-	  * @return ln(x) double type
+	  * @param value double type
+	  * @return ln(value) double type
 	  * @throws Exception the exception
 	  */
-	 public static double ln(double x) throws Exception {
-		 if (x <= 0) {
+	 public static double ln(double value) throws Exception {
+		 if (value <= 0) {
 			 throw new Exception("math range error");
 		 }
 		 double result = 0.0;
-		 while (x > 2) {
+		 while (value > 2) {
 			 result += LN2;
-			 x /= 2;
+			 value /= 2;
 		 }
-		 result += lnBase(x, EXPANSION);
+		 result += lnBase(value, EXPANSION);
 		 return result;
 	 }
 	 
 	 /*
 	  * Ln base.
 	  *
-	  * @param x domain is (0,2],double type
-	  * @param n the number of Taylor expansions,integer type
-	  * @return ln(x) double type
+	  * @param value domain is (0,2],double type
+	  * @param nterms the number of Taylor expansions,integer type
+	  * @return ln(value) double type
 	  */
-	 public static double lnBase(double x, int n) {
-		 x -= 1;
+	 public static double lnBase(double value, int nterms) {
+		 value -= 1;
 		 int factor = 1;
 		 double result = 0.0;
-		 for (int i = 1; i <= n; i++) {
-			 result += factor * power(x, i) / i;
+		 for (int i = 1; i <= nterms; i++) {
+			 result += factor * power(value, i) / i;
 			 factor = -factor;
 		 }
 		 return result;
--- code/src/seon6011/test/test.java
+++ code/src/seon6011/test/test.java
@@ -9,94 +9,89 @@ import org.junit.jupiter.api.Test;
 
 class test {
 
-	/** The Constant EPSON. */
-	public static final double EPSON = 0.0000001; // allowable error
+	/** The Constant epsilon. */
+	public static final double EPSILON = 0.0000001; // allowable error
 	
-	/** Test function y=e^x. */
+	/** Test exponential function calcValue=e^x. */
 	@Test
 	public void testEx() {
-		double y = main.ex(2.4, 20);
-		double expectY = 11.0231763;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.ex(2.4, 20);
+		double actualValue = 11.0231763;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 			
 	/** Test function lnBase. */
 	@Test
 	public void testLnBase() {
-		double y = main.lnBase(0.6, 20);
-	    double expectY = -0.5108256;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.lnBase(0.6, 20);
+	    double actualValue = -0.5108256;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 	
 	/**
 	 * Test function ln.
-	 *
 	 * @throws Exception the exception
 	 */
 	@Test
 	public void testLn() throws Exception {
-		double y = main.ln(10.6);
-	    double expectY = 2.3608540;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.ln(10.6);
+	    double actualValue = 2.3608540;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 	
 	/** Test function power(double a,int x). */
 	@Test
 	public void testIntPower() {
-		double y = main.power(1.6, 7);
-	    double expectY = 26.8435456;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.power(1.6, 7);
+	    double actualValue = 26.8435456;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 	
 	/**
 	 * Test function power(double a,double x).
-	 *
 	 * @throws Exception the exception
 	 */
 	@Test
 	public void testDecimalPower() throws Exception {
-		double y = main.power(5.6, 7.5);
-	    double expectY = 408705.2369134;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.power(5.6, 7.5);
+	    double actualValue = 408705.2369134;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 	
 	/**
 	 * Set a=0, output is 0. When x=0, output is a
-	 *
 	 * @throws Exception the exception
 	 */
 	@Test
 	public void testF5() throws Exception {
-		double y = main.calculate(0, 19, 2);
-	    double expectY = 0;
-	    assertTrue(y == expectY);
-	    y = main.calculate(2, 10, 0);
-	    expectY = 2;
-	    assertTrue(y == expectY);
+		double calcValue = main.calculate(0, 19, 2);
+	    double actualValue = 0;
+	    assertTrue(calcValue == actualValue);
+	    calcValue = main.calculate(2, 10, 0);
+	    actualValue = 2;
+	    assertTrue(calcValue == actualValue);
 	}
 	
 	/**
 	 * Test f5 with positive exponent.
-	 *
 	 * @throws Exception the exception
 	 */
 	@Test
 	public void testF5PositiveX() throws Exception {
-		double y = main.calculate(1.0, 3.4, 5.6);
-	    double expectY = 946.8516393;
-	    System.out.println(y);
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.calculate(1.0, 3.4, 5.6);
+	    double actualValue = 946.8516393;
+	    System.out.println(calcValue);
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 	
 	/**
 	 * Test f5 with negative exponent.
-	 *
 	 * @throws Exception the exception
 	 */
 	@Test
 	public void testF5NegativeX() throws Exception {
-		double y = main.calculate(2.0, 3.4, -5.6);
-	    double expectY = 0.0021122;
-	    assertTrue(main.abs(y - expectY) < EPSON);
+		double calcValue = main.calculate(2.0, 3.4, -5.6);
+	    double actualValue = 0.0021122;
+	    assertTrue(main.abs(calcValue - actualValue) < EPSILON);
 	}
 }
